29de00e4bcb2dd28adacd5b0f3e9fb087f59577a
Thejairex <yairjesus777@gmail.com>
2023-07-07 20:25:06 -0300

    Add queries of orden and executed tables

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from iqoption import Iq
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-mysql=MySQL(app)
+mysql = MySQL(app)
 iq = Iq()
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -12,19 +12,33 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'market'
 app.secret_key = "OtakuTeca"
 
-def fecth_data():
+
+def fetch_ordens():
     cur = mysql.connection.cursor()
-    query = "SELECT * FROM ordens"
+    query = "SELECT * FROM ordens ORDER BY id_orden DESC"
     cur.execute(query)
-    return cur.fetchall()
+    datas = cur.fetchall()
+    return datas
+
+def fecth_result():
+    cur = mysql.connection.cursor()
+    query = "SELECT * FROM executeds ORDER BY id_executed DESC"
+    cur.execute(query)
+    datas = cur.fetchall()
+    return datas
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/ordenes")
 def ordenes():
-    return render_template('ordenes.html', datas = fecth_data())
+    return render_template('ordenes.html', datas = fetch_ordens())
+
+@app.route("/resultados")
+def resultados():
+    return render_template('resultados.html', datas= fecth_result())
 
 
 if __name__ == "__main__":
--- iqoption.py
+++ iqoption.py
@@ -1,14 +1,19 @@
-from iqoptionapi.stable_api import IQ_Option
 import sys
+from dotenv import load_dotenv
+import os
+
+from iqoptionapi.stable_api import IQ_Option
+
+load_dotenv()
 
 
 class Iq:
     def __init__(self) -> bool:
-        self.iq = IQ_Option("yairjesus777@gmail.com","Aiwa_2015")
+        self.iq = IQ_Option(os.getenv("USER"), os.getenv("PASSWD"))
         check, reason = self.iq.connect()
         if check:
             self.iq.change_balance("PRACTICE")
-                    
+
         else:
             sys.exit()
 
